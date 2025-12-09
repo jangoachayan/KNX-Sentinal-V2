680321d4a8e33d89e50bf70bad9eeeb2d14abc6f
Ashwin <ashwin.ashwinsamuel@gmail.com>
2025-12-09 17:38:11 +0300

    Bump version to 1.0.6

--- knx-sentinel-2/app/main.py
+++ knx-sentinel-2/app/main.py
@@ -10,7 +10,7 @@ import os
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("knx-sentinel")
 
-app = FastAPI(title="KNX Sentinel 2", version="0.1.5")
+app = FastAPI(title="KNX Sentinel 2", version="1.0.6")
 
 # Managers
 connection_manager = KNXConnectionManager()
